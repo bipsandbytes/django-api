d28fecb6d9d7ec92ab6d987633543d61686527ef
Bipin Suresh <bipsandbytes@yahoo.com>
2015-03-09 16:29:14 -0700

    Include packaging files

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-api',
+    version='0.1.1',
+    packages=['django_api'],
+    include_package_data=True,
+    license='BSD License',
+    description='Cleanly specify and validate your Django APIs',
+    long_description=README,
+    author='Bipin Suresh',
+    author_email='bipins@alumni.stanford.edu',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
