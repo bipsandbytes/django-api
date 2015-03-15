df99f4ccbb0c5128bd06da83f60881a85f6dbfe1
Bipin Suresh <bipsandbytes@yahoo.com>
2015-03-15 01:35:31 -0700

    Update package info

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-api',
-    version='0.1.2',
+    version='0.1.3',
     packages=['django_api'],
     url='https://github.com/bipsandbytes/django-api',
     include_package_data=True,
